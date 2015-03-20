5e065e69c8fc9dc62eefaf2d824b4e910bc4f2d5
Esko Nuutila <esko.nuutila@aalto.fi>
2015-03-20 17:32:21 +0200

    Refactor animation options.

--- src/mmb/image_generator_thread.py
+++ src/mmb/image_generator_thread.py
@@ -27,14 +27,20 @@ class ImageGeneratorThread(QThread):
         self.steps = steps
         
     def run(self):
+        # Some interesting targets.
+        mandelbrotAntennas = (-0.1638, 1.0353)
+        seahorseValley = (-0.746, -0.1)
+        elephantValley = (0.2855, 0.012)
+        
         # Animation parameters.
+        target = mandelbrotAntennas
         steps = self.steps
         
         def easing(begin, end, steps):
             return np.fromfunction(lambda step: begin-((step/(steps-1)-1.0)**4-1.0)*(end - begin), (steps,), dtype=float)
         
-        x = easing(-0.5, -0.1638, steps)
-        y = easing(0.0, 1.0353, steps)
+        x = easing(-0.5, target[0], steps)
+        y = easing(0.0, target[1], steps)
         w = easing(3, 0.0005, steps)
         mandelbrot = MandelbrotSet(self.width, self.height, self.maxit)
         colorizer = Colorizer(self.maxit)
